8d673dd799da0eb8f4141fda5b1bb8623161fcdb
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-07-11 17:58:19 +0530

    push env

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-require('dotenv').config();
+  require('dotenv').config();
 
-const mongoose = require('mongoose');
-const express = require('express');
-const user_route = require('./routes/userRoute');
-const {sendDailyQuotesEmail} = require('./controllers/dailyQuote'); // Corrected import path
-const cron = require('node-cron');
+  const mongoose = require('mongoose');
+  const express = require('express');
+  const user_route = require('./routes/userRoute');
+  const {sendDailyQuotesEmail} = require('./controllers/dailyQuote'); // Corrected import path
+  const cron = require('node-cron');
+  const PORT=process.env.PORT || 2000
 
-mongoose.connect('mongodb://localhost:27017/nodemailer-app-backend')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB', err);
-  });
+  mongoose.connect('mongodb://localhost:27017/nodemailer-app-backend')
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+      console.error('Error connecting to MongoDB', err);
+    });
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+  app.set('view engine', 'ejs');
+  app.set('views', './views');
 
-app.use('/', user_route);
+  app.use('/', user_route);
 
-app.listen(2000, () => console.log('Server is listening on port 2000'));
+  app.listen(PORT, () => console.log('Server is listening on port 2000'));
 
-// Schedule task to send daily quotes at 11:50 AM every day
-cron.schedule('07 17 * * *', sendDailyQuotesEmail);
+  // Schedule task to send daily quotes at 11:50 AM every day
+  cron.schedule('0 8 * * *', sendDailyQuotesEmail);
